047b9968778ffc19a7dfe1cfddc0050dc6d5bd24
Emine Özlem <emineozlem3301@gmail.com>
2023-12-13 18:49:35 +0000

    ödev için kelime api oluşturuldu.

---
+++ ApiOdev/wordapi.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
+import pandas as pd
+import requests
+
+app = Flask(__name__)
+api = Api(app)
+
+
+
+class GetAllWord(Resource):
+    def get(self):
+
+        url = 'https://api.datamuse.com/words?ml=duck&sp=b*&max=10'
+
+        resp = requests.get(url)
+        data = resp.json()
+
+
+        return {'data': data}, 200
+
+api.add_resource(GetAllWord, '/')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8001)
+    app.run()
+
